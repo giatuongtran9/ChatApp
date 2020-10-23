81b90c4b4b6536aa58018941b13f6985c37cce0d
unknown <trangiatuong777@gmail.com>
2020-10-22 21:11:38 -0400

    Updates

---
+++ precache-manifest.ef00000e2ff794310d2e1a559d23774f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc61560c2ebdd7b8fbacda0eab9cd237",
+    "url": "/ChatApp/index.html"
+  },
+  {
+    "revision": "98a57de90b2f9e718773",
+    "url": "/ChatApp/static/css/main.8f3d490d.chunk.css"
+  },
+  {
+    "revision": "cfd4f220e816229a0669",
+    "url": "/ChatApp/static/js/2.a9b567c0.chunk.js"
+  },
+  {
+    "revision": "7c110bcbe93998abfbde932683d531c0",
+    "url": "/ChatApp/static/js/2.a9b567c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a57de90b2f9e718773",
+    "url": "/ChatApp/static/js/main.b97a4a5d.chunk.js"
+  },
+  {
+    "revision": "b3f2d545ed619914b611",
+    "url": "/ChatApp/static/js/runtime-main.a20c18e1.js"
+  }
+]);
