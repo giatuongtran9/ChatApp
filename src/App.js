12d55d0fecfaf5dd51beb9d189ad2252ae961278
unknown <trangiatuong777@gmail.com>
2020-10-22 21:18:42 -0400

    change to hashbrowser

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
 
-import { BrowserRouter, Switch, Route} from 'react-router-dom'
+import { HashRouter, Switch, Route} from 'react-router-dom'
 
 import Home from './components/Home/Home'
 import ChatRoom from './components/ChatRoom/ChatRoom'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/:roomId/:name" component={ChatRoom} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
