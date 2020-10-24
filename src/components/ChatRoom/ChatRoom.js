cbdfd655d297f1fd7de2d99c814f071e57c1553d
unknown <trangiatuong777@gmail.com>
2020-10-24 14:42:45 -0400

    minor fixed

--- src/components/ChatRoom/ChatRoom.js
+++ src/components/ChatRoom/ChatRoom.js
@@ -23,7 +23,7 @@ const ChatRoom = (props) => {
     }
 
     useEffect(() => {
-        socketRef.current = socketIOClient('http://localhost:8000', {
+        socketRef.current = socketIOClient('/', {
             query: { roomId }
         })
         console.log(socketRef.current)
---
+++ src/server/server.js
@@ -0,0 +1,27 @@
+const server = require("http").createServer();
+const io = require("socket.io")(server);
+
+const PORT = process.env.PORT || 8000;
+
+io.on("connection", (socket) => {
+    const  roomId  = socket.handshake.query['roomId'];
+
+    socket.join(roomId);
+
+    socket.on('new-user', (user) => {
+        console.log(user + ' joins the room')
+        io.in(roomId).emit('new-user', user);
+    });
+
+    socket.on('newChatMessage', (data) => {
+        io.in(roomId).emit('newChatMessage', data);
+    });
+
+    socket.once('disconnect', () => {
+        socket.leave(roomId);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
